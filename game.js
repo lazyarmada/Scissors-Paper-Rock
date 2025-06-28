fcbc83c04aa3960b5755bf452ce9796a072c98ae
KrisDa_One <176632066+lazyarmada@users.noreply.github.com>
2025-06-27 22:15:32 -0400

    Added Function for plarerChoice

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 // The Rule: Rock beats Scissors beats paper beats Rock
 // CPU will choose Rock Paper Scissors at random
 function getComputerChoice() {
-// CPU chooses a number is chosen between 0 and 1
+// CPU randomly chooses a number between 0 and 1
     let cpuNumber = Math.random()
     // if 0 or more, its rock
     // if .33 to .66, its paper
@@ -17,10 +17,45 @@ function getComputerChoice() {
         }
     }
     console.log(getComputerChoice())
+
+const playButton = document.querySelector("#human-input")
+const log = document.querySelector("#log")
+
+// PLayer clicks start game button and prompted to type in choice
+playButton.addEventListener("click", () => {
+    let playerChoice = prompt("Type in Rock, Paper, or Scissors");
+    // if cancel or no input, then player will get "Try Again" error
+    if (playerChoice === null || playerChoice === "") {
+        log.innerText = "ERROR! Try Again!";
+    }
+    // Else continue playChoice
+        else {
+            // let player type in choice of Rock Paper or Scissors.
+            let choice = playerChoice.toLowerCase();
+            // if typed Rock Paper, or Scissors, Log will keep track of playChoice
+            // anything else will make an ERROR message
+            if (choice === "rock") {
+                log.innerText = "You have chosen ROCK!";
+            }
+            else if (choice === "paper") {
+                log.innerText = "You have chosen PAPER!";
+            }
+            else if (choice === "scissors") {
+                log.innerText = "You have chosen SCISSORS!"
+            }
+            else { 
+                log.innerText = "ERROR! Invalid Choice! Please try again!";
+            }
+            console.log(choice)
+        }
+})
+        
+
+
+
 // Player clicks either Rock Paper or Scissors button
-function getPlayerChoice {
+function getPlayerChoice() {
     
 }
 // Game will check what CPU and Player has chosen
 // Game end with whoever chooses the dominate choice
-
