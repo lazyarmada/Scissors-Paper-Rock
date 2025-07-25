2326764a87b5a46a6c60e6c805c3743fa2419e7f
KrisDa_One <176632066+lazyarmada@users.noreply.github.com>
2025-07-25 13:22:12 -0400

    Code created to play a round

--- game.js
+++ game.js
@@ -1,61 +1,78 @@
 // The Rule: Rock beats Scissors beats paper beats Rock
 // CPU will choose Rock Paper Scissors at random
 function getComputerChoice() {
-// CPU randomly chooses a number between 0 and 1
-    let cpuNumber = Math.random()
-    // if 0 or more, its rock
-    // if .33 to .66, its paper
-    // if its more than .66, its scissor
-        if (cpuNumber < 0.33) {
-            return "rock"
-        }
-        else if (cpuNumber < 0.66) {
-            return "paper"
-        }
-        else {
-            return "scissors"
-        }
+ let rps = ["rock", "paper", "scissors"] 
+ let random = rps[Math.floor(Math.random() * rps.length)]
+    return random
+}
+
+// Declare the Start Game button as the getHumanChoice
+const playerButton = document.getElementById("human-input")
+const scoreLog = document.getElementById("log")
+// make a function to getHumanChoice and turn the button into a click prompt
+
+
+function getHumanChoice() {
+    return prompt("Type your choice! Rock, Paper, or Scissors!")
+}
+
+// game will keep score of both player and cpu
+let humanScore = Number(document.getElementById("humanScore").innerText)
+let cpuScore = Number(document.getElementById("cpuScore").innerText)
+// Game will play round by round
+// Round will use human and cpu choices as arguments
+
+function playRound(humanSelection, cpuSelection) { 
+    // humanSelection and cpuSelection is always different
+    // if humanSelection is equal to cpuSelection, it's a tie
+    if (humanSelection === cpuSelection) {
+        // increment both scores
+        humanScore++;
+        cpuScore++;
+        document.getElementById("humanScore").innerText = humanScore;
+        document.getElementById("cpuScore").innerText = cpuScore;
+        return (scoreLog.innerText = "This has ended in a TIE! Try Again!");
     }
-    console.log(getComputerChoice())
+    //increment winner's score and announces winner
 
-const playButton = document.querySelector("#human-input")
-const log = document.querySelector("#log")
-
-// PLayer clicks start game button and prompted to type in choice
-playButton.addEventListener("click", () => {
-    let playerChoice = prompt("Type in Rock, Paper, or Scissors");
-    // if cancel or no input, then player will get "Try Again" error
-    if (playerChoice === null || playerChoice === "") {
-        log.innerText = "ERROR! Try Again!";
+    // if/else if for each time humanSelection beats cpuSelection, increment humanScore by 1
+    else if (humanSelection === "rock" && cpuSelection === "scissors") {
+        humanScore++;
+        document.getElementById("humanScore").innerText = humanScore;
+        return (scoreLog.innerText = "You WIN! Rock beats Scissors!");
     }
-    // Else continue playChoice
-        else {
-            // let player type in choice of Rock Paper or Scissors.
-            let choice = playerChoice.toLowerCase();
-            // if typed Rock Paper, or Scissors, Log will keep track of playChoice
-            // anything else will make an ERROR message
-            if (choice === "rock") {
-                log.innerText = "You have chosen ROCK!";
-            }
-            else if (choice === "paper") {
-                log.innerText = "You have chosen PAPER!";
-            }
-            else if (choice === "scissors") {
-                log.innerText = "You have chosen SCISSORS!"
-            }
-            else { 
-                log.innerText = "ERROR! Invalid Choice! Please try again!";
-            }
-            console.log(choice)
-        }
-})
-        
+    else if (humanSelection === "paper" && cpuSelection === "rock") {
+        humanScore++;
+        document.getElementById("humanScore").innerText = humanScore;
+        return (scoreLog.innerText = "You WIN! Paper beats Rock!");
+    }
+    else if (humanSelection === "scissors" && cpuSelection === "paper") {
+        humanScore++;
+        document.getElementById("humanScore").innerText = humanScore;
+        return (scoreLog.innerText = "You WIN! Scissor beats Paper");
+    }
+    else if (humanSelection === "rock" && cpuSelection === "paper") { 
+        cpuScore++;
+        document.getElementById("cpuScore").innerText = cpuScore;
+        return (scoreLog.innerText = "You LOSE! Paper beats Rock");
+    }
 
-
-
-// Player clicks either Rock Paper or Scissors button
-function getPlayerChoice() {
+    else if (humanSelection === "paper" && cpuSelection === "scissors") {
+        cpuScore++;
+        document.getElementById("cpuScore").innerText = cpuScore;
+        return (scoreLog.innerText = "You LOSE! Scissors beats Paper");
+    }
+    else if (humanSelection === "scissors" && cpuSelection === "rock") {
+        cpuScore++;
+        document.getElementById("cpuScore").innerText = cpuScore;
+        return (scoreLog.innerText = "You LOSE! Rock beats Scissors")
+    }
     
 }
-// Game will check what CPU and Player has chosen
-// Game end with whoever chooses the dominate choice
+
+let humanSelection = getHumanChoice();
+let cpuSelection = getComputerChoice();
+console.log(cpuSelection)
+console.log(playRound(humanSelection, cpuSelection));
+// playGame will be 5 rounds
+// whichever score is higher after 5 rounds is the declared winner, Game end
